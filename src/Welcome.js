7df890de434c441550055146a66cd54b497d0286
Isadora <isadoragravila@gmail.com>
2022-05-11 19:02:07 -0300

    Componente e layout tela-inicio

---
+++ src/Welcome.js
@@ -0,0 +1,9 @@
+export default function Welcome () {
+    return (
+        <div className="tela-inicio">
+            <img src="./images/logo.png" alt="logo" />
+            <h1>ZapRecall</h1>
+            <div className="botao"><p>Iniciar Recall!!</p></div>
+        </div>
+    )
+}
