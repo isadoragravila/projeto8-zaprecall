67b3125650e53c2133792f2f7433c948efd03d3e
Isadora <isadoragravila@gmail.com>
2022-05-11 23:09:28 -0300

    Adição de onClick na tela inicial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
 import Welcome from "./Welcome";
 import Deck from "./Deck";
 
 export default function App () {
+    const [tela, setTela] = React.useState("welcome");
+
     return (
         <>
-            <Deck />
+            {tela === "welcome" ? <Welcome setTela={setTela} /> : <Deck />}
         </>
     );
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,14 +1,9 @@
-export default function Welcome () {
-
-    function openDeck () {
-        console.log("abriu o deck");
-    }
-    
+export default function Welcome ({ setTela }) {
     return (
         <div className="tela-inicio">
             <img src="./images/logo.png" alt="logo" />
             <h1>ZapRecall</h1>
-            <div className="botao" onClick={openDeck}><p>Iniciar Recall!!</p></div>
+            <div className="botao" onClick={() => setTela("deck")}><p>Iniciar Recall!!</p></div>
         </div>
     )
 }
