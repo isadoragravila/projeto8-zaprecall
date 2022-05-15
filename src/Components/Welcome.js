cacbbb4df6e626419977425620c884bc4f486823
Isadora Ávila <isadoragravila@gmail.com>
2022-05-15 13:56:57 -0300

    Bônus meta

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,13 +1,35 @@
+
+import React from "react";
 import logo from "../Assets/images/logo.png";
 
 export default function Welcome ({ setTela }) {
-    return (
-        <div className="tela-inicio">
-            <img src={logo} alt="logo" />
-            <h1>ZapRecall</h1>
-            <div className="botao" onClick={() => setTela("deck")}>
-                <p>Iniciar Recall!!</p>
-            </div>
+  const [botao, setBotao] = React.useState(false);
+
+  function inputValue(valor) {
+    let num = valor.target.value;
+    num = Number(num);
+    if (num >= 1 && num <= 8 && num % 1 === 0) {
+      setMeta(num);
+      setBotao(true);
+    } else {
+      setBotao(false);
+    }
+  }
+    
+  return (
+    <div className="tela-inicio">
+      <img src={logo} alt="logo" />
+      <h1>ZapRecall</h1>
+      <input type="text" className="meta" placeholder="Digite sua meta de zaps..." onKeyUp={(valor) => inputValue(valor)} />
+      {botao === true ? (
+        <div className="botao ativo" onClick={() => setTela("deck")}>
+          <p>Iniciar Recall!!</p>
         </div>
-    );
+      ) : (
+        <div className="botao">
+          <p>Iniciar Recall!!</p>
+        </div>
+      )}
+    </div>
+  );
 }
