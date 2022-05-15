475d0b356e2cdfc06a6fb4f9502bdcee48605c5a
Isadora Ávila <isadoragravila@gmail.com>
2022-05-15 14:05:21 -0300

    Bônus meta

--- src/Components/Final.js
+++ src/Components/Final.js
@@ -1,12 +1,14 @@
 import party from "../Assets/images/party.png";
 import sad from "../Assets/images/sad.png";
 
-export default function Final({ arrIcons }) {
+export default function Final({ arrIcons, meta }) {
   let emoji = party;
   let title = "Parabéns!";
   let phrase = "Você não esqueceu de nenhum flashcard!";
 
-  if (arrIcons.includes("close-circle")) {
+  const certas = arrIcons.filter((icon) => icon === "checkmark-circle");
+  
+  if (arrIcons.includes("close-circle") || certas.length < meta) {
     emoji = sad;
     title = "Putz...";
     phrase = "Ainda faltam alguns... Mas não desanime!";
